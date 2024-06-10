308fbbacdd8fee8f769b8e6c61a2a4afd67a04eb
Nasim Reza <nasim8382@gmail.com>
2024-06-10 19:22:02 +0600

    all products get route done

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -19,6 +19,25 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: "Products fetched successfully!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Something went wrong",
+      error: error,
+    });
+  }
+};
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
 };
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -7,6 +7,12 @@ const createProductIntoDB = async (productData: TProduct) => {
   return result;
 };
 
+const getAllProductsFromDB = async () => {
+  const result = await Product.find();
+  return result;
+};
+
 export const ProductServices = {
   createProductIntoDB,
+  getAllProductsFromDB,
 };
