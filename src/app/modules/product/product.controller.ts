40d963ebf05748cb13c3e8cd7f0bbba48744e26a
Nasim Reza <nasim8382@gmail.com>
2024-06-11 18:57:24 +0600

    order controller file added

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -7,7 +7,7 @@ const createProduct = async (req: Request, res: Response) => {
 
     res.status(200).json({
       success: true,
-      message: "Product created successfully",
+      message: "Product created successfully!",
       data: result,
     });
   } catch (error: any) {
