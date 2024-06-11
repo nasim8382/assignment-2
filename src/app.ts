b4c68a5cb09c0df1e5c0444be1bfab2f037e7161
Nasim Reza <nasim8382@gmail.com>
2024-06-11 20:29:51 +0600

    order created successfully

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { ProductRoutes } from "./app/modules/product/product.route";
+import { OrderRoutes } from "./app/modules/order/order.route";
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-// application routes
+// product routes
 app.use("/api/products", ProductRoutes);
+
+// order routes
+app.use("/api/orders", OrderRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
